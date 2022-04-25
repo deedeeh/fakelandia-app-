8fdbe88db977ec0ab36a53d38a8b47e28c7392b4
deedeeh <dinahafez.8@gmail.com>
2022-04-25 13:15:18 +0100

    Created 3 components - Home, Misdemeanours, Confession -  with a heading

---
+++ src/components/Confession.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Confession: React.FC = () => 
+  <div>
+    <h1>Confession</h1>
+  </div>
+
+export default Confession;
---
+++ src/components/Misdemeanours.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Misdemeanours: React.FC = () => 
+  <div>
+    <h1>Misdemeanours</h1>
+  </div>
+
+export default Misdemeanours;
