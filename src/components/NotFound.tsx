342c636f1ac004d7ba49841b5156d6b7ef0fae75
deedeeh <dinahafez.8@gmail.com>
2022-04-25 13:36:10 +0100

    Added 404 not found component to routes

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NotFound: React.FC = () => 
+  <div>
+    <h1>404 Not Found</h1>
+  </div>
+
+export default NotFound;
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Confession from './Confession';
 import Home from './Home'
 import Misdemeanours from './Misdemeanours';
+import NotFound from './NotFound';
 
 const Router: React.FC = () => (
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/misdemeanours' element={<Misdemeanours />} />
     <Route path='/confession' element={<Confession />} />
+    <Route path='*' element={<NotFound />} />
   </Routes>
 )
 
