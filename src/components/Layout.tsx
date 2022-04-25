5c8d1f113a0ab541c181a81b5d7a146583315720
deedeeh <dinahafez.8@gmail.com>
2022-04-25 14:39:15 +0100

    Created Navigation component and added it to Layout - Added Outlet for components content

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Navigation from './Navigation';
+import { Outlet } from 'react-router-dom';
 
 const Layout: React.FC = () => (
-  <header>
-    <h1>Fakelandia justice department</h1>
-  </header>
+  <>
+    <header>
+      <h1>Fakelandia justice department</h1>
+      <Navigation />
+    </header>
+    <div>
+      <Outlet />
+    </div>
+  </>
 )
 
 export default Layout;
