42e93f9b4ec6ac91c1d81697024a4a343d2aa917
deedeeh <dinahafez.8@gmail.com>
2022-04-25 19:28:17 +0100

    Changed Home component content and added styles to it

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Home: React.FC = () => 
-  <div>
-    <h1>Home</h1>
+  <div className='home-container'>
+    <h1 className='home-heading'>Welcome to the home of the Justice Department of Fakelandia</h1>
+    <p className='home-paragraph'>Here you can browse a list of recent misdemeanours committed by our citizens, or you can confess to your own misdemeanours</p>
   </div>
 
 export default Home;
