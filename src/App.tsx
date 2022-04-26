986ab3c8bf90fd5b52235ccc09566fe2911cd739
deedeeh <dinahafez.8@gmail.com>
2022-04-26 17:29:34 +0100

    Generated Data with useState and useEffect and can access it globally with useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Router from './components/Router';
+import generateMisdemeanours, { IMisdemeanour } from './generate_misdemeanours'; 
+
+export const MisdemeanoursContext = React.createContext<Array<IMisdemeanour>>([]);
 
 function App() {
+  const [ misdemeanours, setMisdemeanours ] = useState<Array<IMisdemeanour>>([]);
+
+  useEffect(() => {
+    getMisdemeanours();
+  }, [])
+
+  const getMisdemeanours = async () => {
+    const generatedMisdemeanours = await generateMisdemeanours(2); 
+    setMisdemeanours(generatedMisdemeanours);
+  }
+
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <MisdemeanoursContext.Provider value={misdemeanours}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </MisdemeanoursContext.Provider>
   )
 }
 
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/misdemeanours.css'
+import { MisdemeanoursContext } from '../App'
 
-const Grid: React.FC = () => (
-  <div className='grid-container'>
-    <div className='item '>Citizen ID</div>
-    <div className='item item--border'>Date</div>
-    <div className='item item--border'>Misdemeanour</div>
-    <div className='item item--border'>Punishment Idea</div>
-  </div>
-)
+const Grid: React.FC = () => {
+  const misdemeanours = useContext(MisdemeanoursContext);
+
+  return (
+    <div className='grid-container'>
+      <div className='table-head'>
+        Citizen ID
+        {misdemeanours.map(misdemeanour => (<div className='cell' key={misdemeanour.citizenId}>{misdemeanour.citizenId}</div>))}
+      </div>
+      <div className='table-head'>
+        Date
+        {misdemeanours.map(misdemeanour => (<div className='cell' key={misdemeanour.citizenId}>{misdemeanour.date}</div>))}
+      </div>
+      <div className='table-head'>
+        Misdemeanour
+        {misdemeanours.map(misdemeanour => (<div className='cell' key={misdemeanour.citizenId}>{misdemeanour.misdemeanour}</div>))}
+      </div>
+      <div className='table-head'>Punishment Idea</div>
+    </div>
+  )
+}
 
 export default Grid;
